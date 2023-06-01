ae4839c35fe2a2a69ba8595eaaf67b410dce081f
Sofia Katzman <katzmansof@gmail.com>
2023-05-31 20:59:13 -0400

    finished lab

---
+++ lib/migrations/versions/54da70f23d2d_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 54da70f23d2d
+Revises: 868507567304
+Create Date: 2023-05-31 20:54:02.067182
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '54da70f23d2d'
+down_revision = '868507567304'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ lib/migrations/versions/868507567304_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 868507567304
+Revises: 
+Create Date: 2023-05-31 20:53:55.618298
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '868507567304'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
